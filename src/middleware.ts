b048c39d5e6aa0580110d3dd7746c3273772fb53
Alberto Conceição <albertodgc@gmail.com>
2026-01-15 13:49:26 -0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -49,7 +49,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
-};
+    matcher: [
+      "/dashboard/:path*",
+    ],
+  };
+  
